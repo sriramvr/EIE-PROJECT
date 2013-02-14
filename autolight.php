df240b09c0a55f93f6a28d28d3222936e3101a32
sriramvr <send2sriram@yahoo.in>
2013-02-14 07:12:45 -0800

    Create autolight.php

---
+++ autolight.php
@@ -0,0 +1,42 @@
+
+<?php
+/* Simple serial script for automating an industry using raspberry pi 
+and arduino
+*/ 
+     
+    //Load the serial port class 
+    require("php_serial.class.php"); 
+     
+    //Initialize the class 
+    $serial = new phpSerial(); 
+
+    //Specify the serial port to use... in this case COM1 
+    $serial->deviceSet("COM3"); 
+    //Set the serial port parameters. The documentation says 9600 8-N-1, so 
+    $serial->confBaudRate(9600); //Baud rate: 9600
+   // $serial->confParity("none");  //Parity (this is the "N" in "8-N-1") 
+  // $serial->confCharacterLength(8); //Character length (this is the "8" in "8-N-1") 
+   // $serial->confStopBits(1);  //Stop bits (this is the "1" in "8-N-1") 
+    //$serial->confFlowControl("none");
+//Device does not support flow control of any kind, 
+//so set it to none. 
+    //Now we "open" the serial port so we can write to it 
+    $serial->deviceOpen(); 
+    $serial->sendMessage("A"); 
+    //We're done, so close the serial port again 
+ echo "<font size= 10 face='Arial'  color= violet >"; 
+     echo(date("D F d Y"));
+     echo "<br />";
+     echo "<br />";
+      echo "<font size= 6 face='Arial'  color= indigo >"; 
+     echo "  AUTOMATIC MODE TURNED ON \n " ;  
+     echo "<br />";
+     echo "<br />";
+?> 
+<html>
+<body>
+<div style="position: absolute; left: 800; top: 300;"> 
+<a href="http://127.0.0.1/light.php">back</a> 
+</div>
+</body>
+</html>
